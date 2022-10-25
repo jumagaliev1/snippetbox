5efa239e5abda9dca8d84bcdfdc8b8a0e0085c53
jumagaliev1 <200103311@stu.sdu.edu.kz>
2022-10-25 22:56:26 +0600

    :card_file_box: Created Single-record SQL Queries

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -28,7 +28,23 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 
 // Get - return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	// Method execute SQL statement, passing in the untrusted id variable as the value.
+	row := m.DB.QueryRow(stmt, id)
+
+	// Initialize a pointer to a new zeroed Snippet struct.
+	s := &models.Snippet{}
+
+	// Copy the values from each field
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err == sql.ErrNoRows {
+		return nil, models.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
 
 // Latest - return the 10 most recently created snippets.
