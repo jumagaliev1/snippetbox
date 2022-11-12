becefd54980d2de061852d34600fde1c6f9ac298
jumagaliev1 <200103311@stu.sdu.edu.kz>
2022-11-12 20:35:10 +0600

    :bookmark: Testing HTML Forms

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -3,15 +3,28 @@ package main
 import (
 	"github.com/azhumagaliyev/snippetbox/pkg/models/mock"
 	"github.com/golangcollege/sessions"
+	"html"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 )
 
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body []byte) string {
+	matches := csrfTokenRX.FindSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(string(matches[1]))
+}
 func newTestApplication(t *testing.T) *application {
 	templateCache, err := newTemplateCache("./../../ui/html/")
 	if err != nil {
@@ -62,5 +75,21 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byt
 	if err != nil {
 		t.Fatal(err)
 	}
+	return rs.StatusCode, rs.Header, body
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header,
+	[]byte) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Read the response body.
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Return the response status, headers and body.
 	return rs.StatusCode, rs.Header, body
 }
